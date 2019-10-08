afaf3a33d8e74062d2d889d551e2fd95df8547e5
juand <juand@JD_PC>
2019-10-08 12:03:04 -0500

    Pruebas Dspic33CH128MP502 - slave Recepcion del slave 6 bytes

---
+++ main.c
@@ -0,0 +1,114 @@
+/**
+  Generated main.c file from MPLAB Code Configurator
+
+  @Company
+    Microchip Technology Inc.
+
+  @File Name
+    main.c
+
+  @Summary
+    This is the generated main.c using PIC24 / dsPIC33 / PIC32MM MCUs.
+
+  @Description
+    This source file provides main entry point for system initialization and application code development.
+    Generation Information :
+        Product Revision  :  PIC24 / dsPIC33 / PIC32MM MCUs - 1.135.0
+        Device            :  dsPIC33CH128MP508
+    The generated drivers are tested against the following:
+        Compiler          :  XC16 v1.36b
+        MPLAB 	          :  MPLAB X v5.20
+*/
+
+/*
+    (c) 2019 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
+*/
+
+/*
+  Section: Included Files
+*/
+
+#include "mcc_generated_files/mcc.h"
+#include "lcd.h"
+ 
+#define DATA_UNDER_TEST 0xAAAA
+ 
+int main(void)
+{
+    // initialize the device
+    SYSTEM_Initialize();
+    LCD_Initialize();
+    
+ 
+    //Program and enable slave
+    SLAVE1_Program();
+    SLAVE1_Start();
+ 
+    ProtocolA_DATA dataReceive;
+ 
+    dataReceive.ProtocolA[0] = 0;
+ 
+    
+    //printf("\fComunication Control \r\n");
+ 
+    //Wait for interrupt from slave
+    while(!SLAVE1_IsInterruptRequested());
+    SLAVE1_InterruptRequestAcknowledge();
+    while(SLAVE1_IsInterruptRequested());
+    SLAVE1_InterruptRequestAcknowledgeComplete();   
+ 
+    //Mailbox read
+    SLAVE1_ProtocolARead((ProtocolA_DATA*)&dataReceive);
+    
+ 
+    while (1)
+    {
+            if(SLAVE1_IsInterruptRequested()){
+            SLAVE1_InterruptRequestAcknowledge();
+            while(SLAVE1_IsInterruptRequested());
+            
+            
+            SLAVE1_ProtocolARead((ProtocolA_DATA*)&dataReceive);
+            
+            //printf("\fValue:%d(c)\r\n",dataReceive.ProtocolA[0]);
+            //DELAY_milliseconds(40);
+            SLAVE1_InterruptRequestAcknowledgeComplete();  
+            if ((dataReceive.ProtocolA[1] & 0xFF) > 5)
+            {
+                LED6_SetHigh();
+            }
+            else
+            {
+                LED6_SetLow();
+            }
+            } 
+            LED5_Toggle();   
+        
+        //Mailbox read
+        
+        //DELAY_milliseconds(40);
+        
+    }
+}
+/**
+ End of File
+*/
+
